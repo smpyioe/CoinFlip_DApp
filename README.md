6af7b9ad295857235b4a6f9c83c89c3d334e7426
Smpyioe <smpyioe@gmail.com>
2025-11-09 11:51:39 +0100

    coinflip.sol working with unsafe random result generation

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Coin Flip
+
+forge create src/CoinFlip.sol:CoinFlip --rpc-url http://127.0.0.1:8545 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --broadcast
+
+cast send 0x5FbDB2315678afecb367f032d93F642f64180aa3 "deposit()" --value 1ether --rpc-url http://127.0.0.1:8545 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+
+cast balance 0x5FbDB2315678afecb367f032d93F642f64180aa3 --rpc-url http://127.0.0.1:8545
+
+cast logs --address 0x5FbDB2315678afecb367f032d93F642f64180aa3 "GameResult(address,bool,uint8)" --rpc-url http://127.0.0.1:8545
--- backend/src/CoinFlip.sol
+++ backend/src/CoinFlip.sol
@@ -2,6 +2,11 @@
 pragma solidity ^0.8.13;
 
 contract CoinFlip {
+    enum CoinGuess{
+        Heads,
+        Tails
+    }
+
     mapping(address => uint256) public balances;
 
     function deposit() external payable {
@@ -13,5 +18,26 @@ contract CoinFlip {
         require(balances[msg.sender] >= amount, "Not enough balance");
         balances[msg.sender] -= amount;
         payable(msg.sender).transfer(amount);
+    }
+
+    event GameResult(address player, bool win, CoinGuess guess);
+
+    function flip(CoinGuess guess, uint256 amount) external {
+        require(balances[msg.sender]>=amount, "Not enough balance");
+        CoinGuess result = CoinGuess(uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender))) % 2);
+
+        bool win = (result==guess);
+        if (win){
+            balances[msg.sender] += amount;
+        }
+        else{
+            balances[msg.sender] -= amount;
+        }
+
+        emit GameResult(msg.sender, win, guess);
+    }
+
+    function getBalance(address check) view external returns (uint256){
+        return balances[check];
     }
 }
