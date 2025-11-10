266f69a0091fb735dfbd562a86013bb47326fe09
Smpyioe <smpyioe@gmail.com>
2025-11-10 20:26:13 +0100

    create frontend folder

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-center">
+        <h1 className="text-5xl font-bold text-white mb-4">
+          Coin Flip Game
+        </h1>
+        <p className="text-gray-400 text-lg">
+          Test your luck on the blockchain
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
